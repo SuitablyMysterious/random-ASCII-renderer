64d3fd3a0798ab4243595e9f2f8251933aa919e4
Suitably Mysterious <fly2be@outlook.com>
2026-03-25 10:06:52 +0000

    Minor changes

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ configFilePath = "config.json"
 with open(configFilePath) as f:
     configFile = json.load(f)
 
+# Define the PNG reader, and load the file
+
+pngReader = png.Reader(configFile["imagePath"])
+width, height, pixels, metadata = pngReader.read()
+
+# Turn imported pixels into a list
+
+pixelList = list(pixels)
